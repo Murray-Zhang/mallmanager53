0751441e76f74293c49804e44278b321ded4185f
张慧政 <394401270@qq.com>
2020-08-20 10:19:19 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,22 @@
 <template>
     <el-container class="container">
-      <el-header class="header">Header</el-header>
+      <el-header class="header">
+        <el-row>
+          <el-col :span="4">
+            <div class="">
+              <img src="../../assets/logo.png" alt="无法显示图片">
+            </div>
+          </el-col>
+          <el-col :span="16" class="middle">
+            <h3>电商后台管理系统</h3>
+          </el-col>
+          <el-col :span="4">
+            <div class="grid-content bg-purple">
+              <a href="" class="loginout">退出</a>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
       <el-container>
         <el-aside class="aside">Aside</el-aside>
         <el-main class="main">Main</el-main>
@@ -26,5 +42,12 @@ export default {
   }
   .main{
     background: #e9eef3;
+  }
+  .middle{
+    text-align: center;
+  }
+  .loginout{
+    line-height: 60px;
+    text-decoration: none;
   }
 </style>
